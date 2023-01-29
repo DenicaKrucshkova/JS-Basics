100707cbea3eaf821c7d1e5a800f96ff8b0b6e5b
Denica Krushcova <117442848+DenicaKrucshkova@users.noreply.github.com>
2023-01-29 10:22:31 +0200

    Add files via upload

---
+++ areaOfFigures.js
@@ -0,0 +1,26 @@
+function areaOfFigures(input) {
+    let figure = input[0];
+   
+    if (figure === "square") {
+        let a = Number(input[1]);
+        console.log((a * a).toFixed(3));
+
+
+    } else if (figure === "rectangle") {
+        let a = Number(input[1]);
+        let b = Number(input[2]);
+        console.log((a * b).toFixed(3));
+
+    } else if (figure === "circle") {
+        let = Number(input[1]);
+        console.log((Math.PI * r*r).toFixed(3));
+
+    } else if (figure === "triangle") {
+        let a = Number(input[1]);
+        let b = Number(input[2]);
+        console.log((a * b / 2).toFixed(3));
+
+    }
+
+}
+areaOfFigures(["square", "5"]);
