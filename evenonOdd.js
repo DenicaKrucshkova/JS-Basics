0c460f77e1867e7b6675c770688d05feaf1f5fa7
Denica Krushcova <117442848+DenicaKrucshkova@users.noreply.github.com>
2023-01-29 10:22:45 +0200

    Add files via upload

---
+++ evenonOdd.js
@@ -0,0 +1,11 @@
+function evenonOdd(input) {
+    let evenonOdd = Number(input[0]);
+
+    if (evenonOdd % 2 === 0) {
+        console.log("even");
+
+    } else {
+        console.log("odd");
+    }
+}
+evenonOdd([7]);
