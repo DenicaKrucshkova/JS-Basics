aa66217b7b3c8d7b4602d3595b80741f825b4cf8
Denica Krushcova <117442848+DenicaKrucshkova@users.noreply.github.com>
2023-02-02 19:24:04 +0200

    Add files via upload

---
+++ petShpp.js
@@ -0,0 +1,10 @@
+function petShpp(input){
+    let dogFood=Number(input[0]);
+        let catFood=Number(input[1]);
+        let total=dogFood*2.5+catFood*4;
+       
+        let print=total+"lv.";
+        console.log(total);
+
+}
+petShpp(["13","9"]);
