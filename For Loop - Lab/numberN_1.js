71f6fd90e103b8754e74dc32b6cda60716cdef79
Denica Krushcova <117442848+DenicaKrucshkova@users.noreply.github.com>
2023-02-17 08:02:24 +0200

    Add files via upload

---
+++ For Loop - Lab/numberN_1.js
@@ -0,0 +1,8 @@
+function numberN_1 (input){
+    let n=Number(input[0]);
+    for (let i = n; i >=1; i--){
+        console.log(i);
+       
+    }
+}
+numberN_1(["2"])
---
+++ For Loop - Lab/numbersFrom_1_100.js
@@ -0,0 +1,8 @@
+function numbersFrom_1_100(input){
+
+
+        for (let  i = 1; i <= 100; i++) {
+            console.log(i);
+
+        }
+    }
